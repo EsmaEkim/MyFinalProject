4dc334d9fceac4d4d6dc9ebf3db98df49bc90e3b
Esma Ekim <esmaekim123@gmail.com>
2024-10-03 00:56:56 +0300

    Introduction to Web API Programming

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Produkt hinzugefügt";
+        public static string ProductNameInvalid = "Produktname ungültig";
+        public static string MaintenanceTime = "Systemwartung";
+        public static string ProductsListed = "Produkte aufgelistet";
+    }
+}
