0caab087e09b865055add897898bd9772b91a547
Esma Ekim <esmaekim123@gmail.com>
2024-10-12 21:44:12 +0300

    API

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -37,7 +37,7 @@ namespace Business.Concrete
         {
             //Jobcodes
             //z.B Hat der Benutzer Berechtigung?
-            if (DateTime.Now.Hour==22)
+            if (DateTime.Now.Hour==6)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
