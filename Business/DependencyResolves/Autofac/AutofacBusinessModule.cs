035e74df03be0a31e58fa9b48321b51f7065f9d6
Esma Ekim <esmaekim123@gmail.com>
2024-11-06 15:53:09 +0300

    Autofac

---
+++ Business/DependencyResolves/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.DependencyResolves.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EfProductDal>().As<IProductService>().SingleInstance();
+        }
+    }
+}
