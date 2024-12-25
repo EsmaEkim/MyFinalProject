e9f606789a3b4b03b96d7cacbdfead67c621443b
Esma Ekim <esmaekim123@gmail.com>
2024-12-25 23:18:38 +0300

    Refactoring

--- Business/ValidationRules/FluentValidation/ProductValidator.cs
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -14,7 +14,7 @@ namespace Business.ValidationRules.FluentValidation
             RuleFor(p => p.ProductName).MinimumLength(2);
             RuleFor(P => P.UnitPrice).NotEmpty();
             RuleFor(P => P.UnitPrice).GreaterThan(0);
-            RuleFor(P => P.UnitPrice).GreaterThanOrEqualTo(10).When(p => p.CategoryId == 1);
+            RuleFor(P => P.UnitPrice).GreaterThanOrEqualTo(10).When(p => p.CategoryId == 1).WithMessage("Der Einzelpreis muss größer als 10 Euro sein.");
             RuleFor(p => p.ProductName).Must(StartWithA).WithMessage("Produkte müssen mit dem Buchstaben A beginnen");
         }
 
